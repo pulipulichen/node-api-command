4af119cd781ffddf72c7f54021df58e4c61f54b8
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-04-15 21:18:51 +0800

    Sat Apr 15 09:18:51 PM CST 2023

---
+++ index.js
@@ -0,0 +1,41 @@
+var http = require('http'); // Import Node.js core module
+
+const execAsync = require("./lib/execAsync");
+const sleep = require("./lib/sleep");
+
+
+var server = http.createServer(async function (req, res) {   //create web server
+    if (req.url == '/') { //check the URL of the current request
+        
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        
+        // xset dpms force off
+        // set response content    
+        res.write('ok');
+
+        try {
+            await execAsync("netstat -tulpn | grep 7860");
+
+            // 表示已經佔用，準備關閉所有docker
+            await execAsync("docker stop $(docker ps -a -q)");
+            await sleep(30000)
+        }
+        catch (e) {
+            // 沒被佔用
+            // console.log(e)
+        }
+            
+
+        await execAsync("xset dpms force off"); // 關螢幕
+        // await execAsync("systemctl hibernate"); // 休眠
+
+        res.end();
+
+    }
+});
+
+let port = 63000
+server.listen(port); //6 - listen for any incoming requests
+
+console.log(`Node.js web server at port ${port} is running..`)
